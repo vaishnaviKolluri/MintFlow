a131aa77f9c867404f0cdf80e1fb24674939cda9
Dan-exe <70999908+Dan-exe@users.noreply.github.com>
2026-05-10 12:14:00 +1000

    swap home view for main tab navigation

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -1,26 +1,22 @@
-// ContentView.swift
-// MintFlow
-//
-
 import SwiftUI
 
 struct ContentView: View {
 
-    @EnvironmentObject var appViewModel: AppViewModel
+	@EnvironmentObject var appViewModel: AppViewModel
 
-    var body: some View {
-        Group {
-            switch appViewModel.currentScreen {
-            case .landing:
-                LandingView()
-            case .login:
-                LoginView()
-            case .signup:
-                SignupView()
-            case .home:
-                HomeView()
-            }
-        }
-        .animation(.easeInOut(duration: 0.35), value: appViewModel.currentScreen)
-    }
+	var body: some View {
+		Group {
+			switch appViewModel.currentScreen {
+			case .landing:
+				LandingView()
+			case .login:
+				LoginView()
+			case .signup:
+				SignupView()
+			case .home:
+				MainTabView()
+			}
+		}
+		.animation(.easeInOut(duration: 0.35), value: appViewModel.currentScreen)
+	}
 }
